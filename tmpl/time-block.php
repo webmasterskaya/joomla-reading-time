2d8f0ad1aac1f125e9a2562b57f435d775740f7b
Artem Vasilev <kern.usr@gmail.com>
2020-04-04 15:17:02 +0300

    Вернул в разработку

--- tmpl/time-block.php
+++ tmpl/time-block.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    Joomla - Reading time
- * @version    1.0.0
+ * @version    __DEPLOY_VERSION__
  * @author     Artem Vasilev - webmasterskaya.xyz
  * @copyright  Copyright (c) 2018 - 2020 Webmasterskaya. All rights reserved.
  * @license    GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
